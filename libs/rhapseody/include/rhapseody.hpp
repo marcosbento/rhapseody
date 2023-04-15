ec98e4ce758b4dbc0c7fae51665447fbba9e2058
Marcos Bento <mb@adlibx.io>
2023-04-15 08:53:49 +0200

    Add minimal library content

---
+++ libs/rhapseody/include/rhapseody.hpp
@@ -0,0 +1,9 @@
+/*
+ * This file is part of the raphsEOdy project.
+ *
+ * Copyright (c) 2023 Marcos Bento (mb at adlibx dot io)
+ *
+ * Distributed under the Boost Software License, Version 1.0.
+ * (See file LICENSE.bsl.txt or find a copy at http://www.boost.org/LICENSE_1_0.txt)
+ *
+ **/
